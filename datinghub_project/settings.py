68e8edda53525688c5f26cd30c65c502605a8ef9
blacksheepthe2nd <blacksheepthe2nd@gmail.com>
2025-12-27 10:05:59 +1100

    Add Brevo email configuration

--- datinghub_project/settings.py
+++ datinghub_project/settings.py
@@ -91,6 +91,18 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email Configuration - Brevo (formerly Sendinblue)
+EMAIL_BACKEND = config(
+    'EMAIL_BACKEND',
+    default='django.core.mail.backends.smtp.EmailBackend'
+)
+EMAIL_HOST = config('EMAIL_HOST', default='smtp-relay.brevo.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='info@dating-hub.com.au')
+
 # Simple .env file
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
